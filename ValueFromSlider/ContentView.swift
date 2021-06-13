0fae16bcfef9490e4214a6caaaa3c42f7fc91651
Philip4000 <6427676@bk.ru>
2021-06-13 22:59:47 +0300

    Initial Commit

---
+++ ValueFromSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValueFromSlider
+//
+//  Created by Philip Noskov on 13.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
